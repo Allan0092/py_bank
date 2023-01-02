fdbc0f3513f5bda3edf7e3c1954aaef85aed9763
Allan0092 <allan.gtm92@gmail.com>
2023-01-02 12:32:04 +0545

    added fg=black on Labels

--- front.py
+++ front.py
@@ -10,14 +10,14 @@ win.maxsize(700,700)
 win.config(background="white")
 
 def login_page():
-    Label(win,text="Py Bank",font=10,bg="white").place(x=300,y=50)
+    Label(win,text="Py Bank",font=10,bg="white",fg="black").place(x=300,y=50)
 
-    Label(win,text="username",bg="white").place(x=80,y=200)
+    Label(win,text="username",bg="white",fg="black").place(x=80,y=200)
     _login=Entry(win).place(x=230,y=200)
-    Label(win,text="password",bg="white").place(x=80,y=250)
+    Label(win,text="password",bg="white",fg="black").place(x=80,y=250)
     _password=Entry(win,show='*').place(x=230,y=250)
     Button(win,text='login').place(x=320,y=330)
-    Label(win,text='or',bg="white").place(x=350,y=400)
+    Label(win,text='or',bg="white",fg="black").place(x=350,y=400)
     Button(win,text="sign up",command=sign_up).place(x=305,y=450)
 
 def sign_up():
@@ -25,19 +25,19 @@ def sign_up():
     win2.minsize(900,900)
     win2.config(background='white')
 
-    Label(win2,text="Sign up",font=100,bg='white').place(x=400,y=80)
+    Label(win2,text="Sign up",font=100,bg='white',fg="black").place(x=400,y=80)
 
-    Label(win2,text='first name',bg="white").place(x=150,y=200)
+    Label(win2,text='first name',bg="white",fg="black").place(x=150,y=200)
     _fname=Entry(win2).place(x=300,y=200)
-    Label(win2,text='middle name',bg="white").place(x=115,y=280)
+    Label(win2,text='middle name',bg="white",fg="black").place(x=115,y=280)
     _mname=Entry(win2).place(x=300,y=280)
-    Label(win2,text='last name',bg="white").place(x=155,y=360)
+    Label(win2,text='last name',bg="white",fg="black").place(x=155,y=360)
     _lname=Entry(win2).place(x=300,y=360)
-    Label(win2,text='email id',bg="white").place(x=175,y=440)
+    Label(win2,text='email id',bg="white",fg="black").place(x=175,y=440)
     _email=Entry(win2).place(x=300,y=440)
-    Label(win2,text='password',bg="white").place(x=155,y=520)
+    Label(win2,text='password',bg="white",fg="black").place(x=155,y=520)
     _passwrd=Entry(win2,show='*').place(x=300,y=520)
-    Label(win2,text='confirm password',bg="white").place(x=50,y=600)
+    Label(win2,text='confirm password',bg="white",fg="black").place(x=50,y=600)
     _cpasswrd=Entry(win2,show='*').place(x=300,y=600)
     
     Button(win2,text='submit').place(x=400,y=700)
