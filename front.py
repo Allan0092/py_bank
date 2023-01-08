450b309d28cea548462e1c4a08ba40874b178729
Allan0092 <allan.gtm92@gmail.com>
2023-01-08 08:36:35 +0545

    finished replacement on sign up

--- front.py
+++ front.py
@@ -13,16 +13,16 @@ def login_page():
     """
         First login page
     """
-    Label(win,text="Py Bank",font=10,bg="white",fg="black").grid(row=0,column=1,padx=150,pady=60)
+    Label(win,text="Py Bank",font=10,bg="white",fg="black").grid(row=0,column=1,pady=60)
 
-    Label(win,text="username",bg="white",fg="black").grid(row=1,column=0)
+    Label(win,text="username",bg="white",fg="black").grid(row=1,column=0,padx=(40,20))
     _login=Entry(win,bg="white",fg='black').grid(row=1,column=1)
 
-    Label(win,text="password",bg="white",fg="black").grid(row=2,column=0)
+    Label(win,text="  password",bg="white",fg="black").grid(row=2,column=0)
     _password=Entry(win,show='*',bg="white",fg='black').grid(row=2,column=1,pady=30)
     
-    Button(win,text='login').grid(row=3,column=1)
-    Label(win,text='or',bg="white",fg="black").grid(row=4,column=1)
+    Button(win,text='login').grid(row=3,column=1,pady=(20,5))
+    Label(win,text='or',bg="white",fg="black").grid(row=4,column=1,pady=10)
     Button(win,text="sign up",command=sign_up).grid(row=10,column=1)
 
 def sign_up():
@@ -36,22 +36,27 @@ def sign_up():
 
     win2.config(background='white')
 
-    Label(win2,text="Sign up",font=100,bg='white',fg="black").place(x=400,y=80)
+    Label(win2,text="Sign up",font=100,bg='white',fg="black").grid(row=0,column=1,pady=60)
 
-    Label(win2,text='first name',bg="white",fg="black").place(x=150,y=200)
-    _fname=Entry(win2,bg="white",fg='black').place(x=300,y=200)
-    Label(win2,text='middle name',bg="white",fg="black").place(x=115,y=280)
-    _mname=Entry(win2,bg="white",fg='black').place(x=300,y=280)
-    Label(win2,text='last name',bg="white",fg="black").place(x=155,y=360)
-    _lname=Entry(win2,bg="white",fg='black').place(x=300,y=360)
-    Label(win2,text='email id',bg="white",fg="black").place(x=175,y=440)
-    _email=Entry(win2,bg="white",fg='black').place(x=300,y=440)
-    Label(win2,text='password',bg="white",fg="black").place(x=155,y=520)
-    _passwrd=Entry(win2,show='*',bg="white",fg='black').place(x=300,y=520)
-    Label(win2,text='confirm password',bg="white",fg="black").place(x=50,y=600)
-    _cpasswrd=Entry(win2,show='*',bg="white",fg='black').place(x=300,y=600)
+    Label(win2,text='            first name',bg="white",fg="black").grid(row=1,column=0)
+    _fname=Entry(win2,bg="white",fg='black').grid(row=1,column=1)
+
+    Label(win2,text='        middle name',bg="white",fg="black").grid(row=2,column=0)
+    _mname=Entry(win2,bg="white",fg='black').grid(row=2,column=1,pady=30)
+
+    Label(win2,text='            last name',bg="white",fg="black").grid(row=3,column=0)
+    _lname=Entry(win2,bg="white",fg='black').grid(row=3,column=1)
+
+    Label(win2,text='            email id',bg="white",fg="black").grid(row=4,column=0)
+    _email=Entry(win2,bg="white",fg='black').grid(row=4,column=1,pady=30)
+
+    Label(win2,text='            password',bg="white",fg="black").grid(row=5,column=0)
+    _passwrd=Entry(win2,show='*',bg="white",fg='black').grid(row=5,column=1)
+
+    Label(win2,text='    confirm password',bg="white",fg="black").grid(row=6,column=0)
+    _cpasswrd=Entry(win2,show='*',bg="white",fg='black').grid(row=6,column=1,pady=30)
     
-    Button(win2,text='submit',bg="white",fg="black").place(x=400,y=700)
+    Button(win2,text='submit',bg="white",fg="black").grid(row=7,column=1,pady=30)
 
 def check_credentials():
     pass
