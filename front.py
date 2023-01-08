d888bd4ef5652a57890c55be3ec095448d16b05c
Allan0092 <allan.gtm92@gmail.com>
2023-01-08 11:09:56 +0545

    added radio buttons

--- front.py
+++ front.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from PIL import ImageTk,Image
+from datetime import date
 import sql_thngs,regex
 
 
@@ -30,11 +31,18 @@ def sign_up():
         sign up function
     """
     win2=Toplevel(win)
-    win2.minsize(900,900)
-    win2.maxsize(900,900)
+    win2.minsize(900,1100)
+    win2.maxsize(900,1100)
     win2.title("Online Banking Signup")
 
     win2.config(background='white')
+
+    gvals=[
+        ("male",'male'),
+        ("female",'female'),
+        ("other",'other')
+    ]
+    gvar=StringVar(win2,'male')
 
     Label(win2,text="Sign up",font=100,bg='white',fg="black").grid(row=0,column=1,pady=60)
 
@@ -50,14 +58,23 @@ def sign_up():
     Label(win2,text='            email id',bg="white",fg="black").grid(row=4,column=0)
     _email=Entry(win2,bg="white",fg='black').grid(row=4,column=1,pady=30)
 
-    Label(win2,text='            password',bg="white",fg="black").grid(row=5,column=0)
-    _passwrd=Entry(win2,show='*',bg="white",fg='black').grid(row=5,column=1)
+    Label(win2,text='      date of birth',bg="white",fg="black").grid(row=5,column=0)
+    _dob=Entry(win2,bg="white",fg='black').grid(row=5,column=1)
 
-    Label(win2,text='    confirm password',bg="white",fg="black").grid(row=6,column=0)
-    _cpasswrd=Entry(win2,show='*',bg="white",fg='black').grid(row=6,column=1,pady=30)
+    Label(win2,text='gender',bg='white',fg='black').grid(row=6,column=0,pady=(30,0))
+    #_gender=Radiobutton(win2,text='male',bg='white',fg='black').grid(row=6,column=1,pady=30)
+    for text,mode in gvals:
+        Radiobutton(win2,fg='black',bg='white',text=text,variable=gvar, value=mode).grid(column=1,pady=5)
+
+    Label(win2,text='            password',bg="white",fg="black").grid(row=12,column=0,pady=(40,0))
+    _passwrd=Entry(win2,show='*',bg="white",fg='black').grid(row=12,column=1)
+
+    Label(win2,text='confirm password',bg="white",fg="black").grid(row=13,column=0,padx=(20,10))
+    _cpasswrd=Entry(win2,show='*',bg="white",fg='black').grid(row=13,column=1,pady=30)
+
+    Button(win2,text='submit',bg="white",fg="black").grid(row=15,column=1,pady=30)
+
     
-    Button(win2,text='submit',bg="white",fg="black").grid(row=7,column=1,pady=30)
-
 def check_credentials():
     pass
 
