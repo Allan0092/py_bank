d487e05e0e28b808bcfc176f16ec63f2b3fed64e
dipesh kumar khadgi <d8khadgi@gmail.com>
2023-01-19 00:25:03 -0500

    added username in sign up

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ win = Tk()
 win.title("Online Banking".center(100))
 win.minsize(2560,1600)
 #win.maxsize(1500,800)
-win.config(background="#90cad1")
+BACKGROUND1="#90cad1"
+BACKGROUND2="white"
+FOREGROUND1='black'
+win.config(background=BACKGROUND1)
 
 IMAGE="images/logo"
 IMAGE2="images/logo2"
@@ -31,24 +34,24 @@ def login_page():
     def clicked_signup():
         frame.grid_forget()
         sign_up()
-    Label(image=my_img,bg="#90cad1").grid(row=0,column=0,rowspan=19,ipady=400)# Py Bank Logo
+    Label(image=my_img,bg=BACKGROUND1).grid(row=0,column=0,rowspan=19,ipady=400)# Py Bank Logo
 
-    Label(image=my_img3,bg="#90cad1").grid(row=0,column=1,pady=(300,0))# Py Bank Text Image
-    #Label(win,text="Py Bank",font=20,bg="#90cad1",fg="black").grid(row=0,column=2,pady=60)
+    Label(image=my_img3,bg=BACKGROUND1).grid(row=0,column=1,pady=(300,0))# Py Bank Text Image
+    #Label(win,text="Py Bank",font=20,bg=BACKGROUND1,fg=FOREGROUND1).grid(row=0,column=2,pady=60)
 
-    frame=LabelFrame(win,bg="#90cad1",border=10,padx=100,pady=100)
+    frame=LabelFrame(win,bg=BACKGROUND1,border=10,padx=100,pady=100)
     frame.grid(row=1,column=1)
 
-    Label(frame,text="Username",bg="#90cad1",fg="black",font=15).grid(row=1,column=1,pady=30,padx=30)
-    _username=Entry(frame,bg="white",fg='black')
+    Label(frame,text="Username",bg=BACKGROUND1,fg=FOREGROUND1,font=15).grid(row=1,column=1,pady=30,padx=30)
+    _username=Entry(frame,bg=BACKGROUND2,fg=FOREGROUND1)
     _username.grid(row=1,column=2,padx=(0,80))
     
 
-    Label(frame,text="Password",bg="#90cad1",fg="black",font=15).grid(row=2,column=1)
-    _password=Entry(frame,show='*',bg="white",fg='black').grid(row=2,column=2,padx=(0,80))
+    Label(frame,text="Password",bg=BACKGROUND1,fg=FOREGROUND1,font=15).grid(row=2,column=1)
+    _password=Entry(frame,show='*',bg=BACKGROUND2,fg=FOREGROUND1).grid(row=2,column=2,padx=(0,80))
     
     Button(frame,text='Log In').grid(row=3,column=2,padx=(0,80),pady=30)
-    Label(frame,text='OR',bg="#90cad1",fg="black").grid(row=4,column=2,padx=(0,80))
+    Label(frame,text='OR',bg=BACKGROUND1,fg=FOREGROUND1).grid(row=4,column=2,padx=(0,80))
     Button(frame,text="Sign Up",command=clicked_signup).grid(row=5,column=2,pady=30,padx=(0,80))
 
     
@@ -58,9 +61,9 @@ def sign_up():
         sign up page
     """
 
-    global new_fname,new_lname,new_mname,new_email,new_dob,new_passwrd,new_cpasswrd,gvar
+    global new_fname,new_lname,new_mname,new_email,new_dob,new_passwrd,new_cpasswrd,gvar,new_uname
 
-    frame2=LabelFrame(win,bg="#90cad1",border=10,padx=10,pady=10)
+    frame2=LabelFrame(win,bg=BACKGROUND1,border=10,padx=10,pady=10)
 
     gvals=[
         ("Male",'male'),
@@ -70,47 +73,49 @@ def sign_up():
     gvar=StringVar(frame2,'None')
     frame2.grid(row=1,column=1)
 
-    #Label(frame2,image=my_img2,bg="#90cad1").grid(row=0,column=1,pady=(45,0))
+    #Label(frame2,image=my_img2,bg=BACKGROUND1).grid(row=0,column=1,pady=(45,0))
 
-    Label(frame2,text="Sign up",font=100,bg='#90cad1',fg="black").grid(row=1,column=1,pady=(20,60),sticky="W")
+    Label(frame2,text="Sign up",font=100,bg='#90cad1',fg=FOREGROUND1).grid(row=1,column=1,pady=(20,60),sticky="W")
 
-    Label(frame2,text='            First name',bg="#90cad1",fg="black").grid(row=2,column=0)
-    new_fname=Entry(frame2,bg="white",fg='black')
+    Label(frame2,text='            First name',bg=BACKGROUND1,fg=FOREGROUND1).grid(row=2,column=0)
+    new_fname=Entry(frame2,bg=BACKGROUND2,fg=FOREGROUND1)
     new_fname.grid(row=2,column=1,padx=(0,100))
 
-    Label(frame2,text='        Middle name',bg="#90cad1",fg="black").grid(row=3,column=0)
-    new_mname=Entry(frame2,bg="white",fg='black')
+    Label(frame2,text='        Middle name',bg=BACKGROUND1,fg=FOREGROUND1).grid(row=3,column=0)
+    new_mname=Entry(frame2,bg=BACKGROUND2,fg=FOREGROUND1)
     new_mname.grid(row=3,column=1,pady=30,padx=(0,100))
 
-    Label(frame2,text='            Last name',bg="#90cad1",fg="black").grid(row=4,column=0)
-    new_lname=Entry(frame2,bg="white",fg='black')
+    Label(frame2,text='            Last name',bg=BACKGROUND1,fg=FOREGROUND1).grid(row=4,column=0)
+    new_lname=Entry(frame2,bg=BACKGROUND2,fg=FOREGROUND1)
     new_lname.grid(row=4,column=1,padx=(0,100))
 
-    Label(frame2,text='            Email Id',bg="#90cad1",fg="black").grid(row=5,column=0)
-    new_email=Entry(frame2,bg="white",fg='black')
+    Label(frame2,text='            Email Id',bg=BACKGROUND1,fg=FOREGROUND1).grid(row=5,column=0)
+    new_email=Entry(frame2,bg=BACKGROUND2,fg=FOREGROUND1)
     new_email.grid(row=5,column=1,pady=30,padx=(0,100))
 
-    
-
-    Label(frame2,text='      Date of Birth',bg="#90cad1",fg="black").grid(row=6,column=0)
-    new_dob=DateEntry(frame2,bg="white",fg='black')
+    Label(frame2,text='      Date of Birth',bg=BACKGROUND1,fg=FOREGROUND1).grid(row=6,column=0)
+    new_dob=DateEntry(frame2,bg=BACKGROUND2,fg=FOREGROUND1)
     new_dob.grid(row=6,column=1,ipadx=40,sticky="W")
 
-    Label(frame2,text='Gender     ',bg='#90cad1',fg='black').grid(row=7,column=0,pady=(30,0),sticky='E')
+    Label(frame2,text='Gender     ',bg='#90cad1',fg=FOREGROUND1).grid(row=7,column=0,pady=(30,0),sticky='E')
     _rowcnt=7
     for text,mode in gvals:
-        Radiobutton(frame2,fg='black',bg='#90cad1',text=text,variable=gvar, value=mode).grid(row=_rowcnt,column=1,sticky='WS',pady=5)
+        Radiobutton(frame2,fg=FOREGROUND1,bg='#90cad1',text=text,variable=gvar, value=mode).grid(row=_rowcnt,column=1,sticky='WS',pady=5)
         _rowcnt+=1
 
-    Label(frame2,text='            Password',bg="#90cad1",fg="black").grid(row=12,column=0,pady=(40,0))
-    new_passwrd=Entry(frame2,show='*',bg="white",fg='black')
+    Label(frame2,text="             Username",bg=BACKGROUND1,fg=FOREGROUND1).grid(row=10,column=0,pady=(40,0))
+    new_uname=Entry(frame2,bg=BACKGROUND2,fg=FOREGROUND1)
+    new_uname.grid(row=10,column=1,sticky='W',pady=(40,0))
+
+    Label(frame2,text='            Password',bg=BACKGROUND1,fg=FOREGROUND1).grid(row=12,column=0,pady=(40,0))
+    new_passwrd=Entry(frame2,show='*',bg=BACKGROUND2,fg=FOREGROUND1)
     new_passwrd.grid(row=12,column=1,padx=(0,100),pady=(40,0))
 
-    Label(frame2,text='Confirm Password',bg="#90cad1",fg="black").grid(row=13,column=0,padx=(20,10))
-    new_cpasswrd=Entry(frame2,show='*',bg="white",fg='black')
+    Label(frame2,text='Confirm Password',bg=BACKGROUND1,fg=FOREGROUND1).grid(row=13,column=0,padx=(20,10))
+    new_cpasswrd=Entry(frame2,show='*',bg=BACKGROUND2,fg=FOREGROUND1)
     new_cpasswrd.grid(row=13,column=1,pady=30,padx=(0,100))
     
-    Button(frame2,text='Submit',bg="white",fg="black",command=sign_up_data).grid(row=15,column=1,pady=30,padx=(0,100))
+    Button(frame2,text='Submit',bg=BACKGROUND2,fg=FOREGROUND1,command=sign_up_data).grid(row=15,column=1,pady=30,padx=(0,100))
 
 def sign_up_data():
     print(f'First name {new_fname.get()}')
