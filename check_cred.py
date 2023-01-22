a187bb0aae53c7d96e4aaa9ca05389cb01308edc
Allan0092 <allan.gtm92@gmail.com>
2023-01-22 13:43:08 +0545

    added syntax checker function

--- check_cred.py
+++ check_cred.py
@@ -1,27 +1,82 @@
-import re
+from email_validator import validate_email, EmailNotValidError
 from faker import Faker
+from datetime import datetime
 
+def valid_email(__email) :
+    """
+        checks for valid email
+    """
+    try:
+        validate = validate_email(__email)
+        __email = validate["email"] 
+        return True, -1, None
+    except EmailNotValidError:
+        return False, 3, ' Not Found'
 
-def check_email(_email):
-    c=re.fullmatch("^[a-z]([a-z]|[0-9])@[a-z].[a-z]",_email)
-    return c 
+def valid_dob(__dob):
+    """
+        checks if given valid date of birth
+    """
+    if not __dob[-4:].isdigit() or __dob[-5]!='/' or not __dob[0].isdigit()or not __dob[-6].isdigit():#checks year, first digit and "/"
+        return False, 4, ' not Valid'
 
-def check_name(_name):
-    c=re.fullmatch("[a-z]+",_name)
-    return c
+    the_rest=__dob[:-5]
+    length=len(the_rest)
+    if length==3:# checks "1/2"
+        if the_rest[0].isdigit()==False or the_rest[1]!='/' or not the_rest[2].isdigit():
+            return False, 4, ' not Valid'
+    elif length==4:# checks "1/23" or "12/3"
+        if the_rest[1]=='/':
+            if not the_rest[2].isdigit() or not the_rest[3].isdigit():
+                return False, 4, ' not Valid'
+        elif the_rest[2]=='/':
+            if not the_rest[1].isdigit() or not the_rest[3].isdigit():
+                return False, 4, ' not Valid'
+        else:
+            return False, 4, ' not Valid'
+    elif length==5:# checks "12/34"
+        if the_rest[2]!='/':
+            return False, 4, ' not Valid'
+        if not the_rest[:2].isdigit() or not the_rest[3:5].isdigit():
+            return False, 4, ' not Valid'
+    return True, -1, None
 
-def check_password(_password):
-    c=re.fullmatch("",_password)
+def valid_username(__username):
+    
+    for letter in __username:
+        if not letter.isalnum():
+            if letter not in ['_','-','@','.']:
+                return False, 5, " not valid"
+    return True, -1, None
+
+def valid_password(__passwrd):
+    variety=[False,False,False,False]# [Uppercase, Lowercase, number, symbol]
+    for char in __passwrd:
+        if char.isalpha():
+            if char.isupper():
+                variety[0]=True
+            elif char.islower():
+                variety[1]=True
+        elif char.isdigit():
+            variety[2]=True
+        elif char==' ':
+            pass
+        else:
+            variety[3]=True
+    for v in variety:
+        if not v:
+            return False, 6, ' not valid'
+    return True, -1, None
 
 def emptyfield_check(information):
     '''
-        Checks if fields are empty
+        Checks for empty fields
     '''
     for n,info in enumerate(information):
         if info=="" or info==None:
-            if n==1:
+            if n==1:# middle name can be empty
                 continue
-            return True
+            return True, n, ' cannot be empty'
     return False
 
 def within_limit_check(information):
@@ -29,33 +84,57 @@ def within_limit_check(information):
         checks if characters are within limit
     '''
     if len(information[0])<2 or len(information[0])>20:# first name
-        return True
+        return True, 0, ' must be between 2-20 letters'
     if len(information[1])>20:# middle name
-        return True
+        return True, 1, ' must be smaller than 20 letters'
     if len(information[2])<2 or len(information[2])>20:# last name
-        return True
+        return True, 2, ' must be between 2-20 letters'
     if len(information[3])<7 or len(information[0])>40:# email
-        return True
+        return True, 3, 'must be between 7-20 characters'
     if len(information[4])<5 or len(information[4])>20:# username
-        return True
-    if len(information[5])<8 or len(information[0])>20:
-        return True
-    return False
+        return True, 5, "must be between 5-20 characters" 
+    if len(information[5])<8 or len(information[0])>20:# password
+        return True, 6, "must be between 8-20 characters"
+    return False, -1, None
 
 def syntax_check(information):
-    pass
+    """
+        checks if valid characters are given
+    """
+    for j in range(3):# First, middle and last name
+        for i in information[j]:
+            _check=i.isalpha()
+            if not _check:
+                return _check, j, ' can only have letters' 
 
-def check_information(args):
+    email_check=valid_email(information[3])# email checker
+    if not email_check[0]:
+        return email_check
+    
+    dob_check=valid_dob(information[4])# date of birth checker
+    if not dob_check[0]:
+        return dob_check
+    
+    username_check=valid_username(information[5])# username checker
+    if not username_check[0]:
+        return username_check
+
+    password_check=valid_password(information[6])# password checker
+    if not password_check[0]:
+        return password_check
+
+
+def main(*args):
     information=list(args)
-    if emptyfield_check(information) and within_limit_check(information):
+    if emptyfield_check(information)[0] and within_limit_check(information)[0]:
         return True
     return False
 
 if __name__=="__main__":
     id = Faker()
 
-    for i in range(200):
+    """for i in range(200):
         a=id.email()
         if check_email(a)==None:
-            print(a)
+            print(a)"""
 
