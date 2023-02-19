f64ff07243125a5dbb8858bfb2ff4535f03c588e
Sworup Timalsina <SworupTimalsina>
2023-02-19 20:51:25 +0545

    added configure, transfer and main functions

---
+++ transfer.py
@@ -0,0 +1,74 @@
+import sqlite3
+
+
+DATABASE="bank_data.db"
+
+def configure():
+    '''
+        creates transaction table.
+    '''
+    conn=sqlite3.connect(DATABASE)
+    c=conn.cursor()
+
+    c.execute("""
+    create table Account(
+        id text,
+        date text,
+        from_acc text,
+        to_acc text,
+        remarks text,
+        status text
+    )
+    """)
+    print("database created successfully")
+    conn.commit()
+    conn.close()
+
+def transfer(info):
+    """
+        Adds a transaction.
+        Arguments:
+            id : 0,
+            date : 1,
+            from_acc : 2,
+            to_acc : 3,
+            remarks : 4,
+            status : 5
+    """
+
+    conn=sqlite3.connect(DATABASE)
+    c=conn.cursor()
+
+    c.execute('INSERT INTO Account VALUES (:id,:date,:from_acc,:to_acc,:remarks, :status)',{
+        'id':info[0],
+        'date':info[1],
+        'from_acc':info[2],
+        'to_acc':info[3],
+        'remarks':info[4],
+        'status':info[8]
+    })
+    print('amount transfered successfully')
+
+    conn.commit()
+    conn.close()
+
+
+
+def main(info):
+    """
+        main function.
+        Arguments:
+            id : 0,
+            date : 1,
+            from_acc : 2,
+            to_acc : 3,
+            remarks : 4,
+            status : 5
+    """
+    
+
+
+try:
+    configure()
+except sqlite3.OperationalError:
+    pass
