cf935f35c89864be108cd1bb97e0c98ed6091dbc
dipesh kumar khadgi <d8khadgi@gmail.com>
2023-02-20 03:32:57 -0500

    added balance feature

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ def login_check():
     """
         Checks username and password
     """
-    global logged_in_name
+    global logged_in_name,client_details
 
     try:# Removes any previous error if it's made 
         login_error1.grid_forget()
@@ -63,7 +63,7 @@ def login_check():
 
     all_clients=DataActions.retrieve_all()# gets all data from table Account
 
-    for client in all_clients:# gets individual client details in format [('Fname', 'Mname', 'Lname', 'Email', 'dateOfBirth', 'Gender', 'Username', 'Password', oid)]
+    for client in all_clients:# gets individual client details in format [('Fname', 'Mname', 'Lname', 'Email', 'dateOfBirth', 'Gender', 'Username', 'Password', 'balance', oid)]
         if login_username.get()==client[6]:# Checks if given username exits.
             if login_password.get()==client[7]:# Checks if username's password matches.
                 """
@@ -73,11 +73,11 @@ def login_check():
                 py_bank_logo.grid_forget()
                 py_bank_title.grid_forget()
                 logged_in_name=f"{client[0]} {client[2]}"
-                homepage()    
+                homepage(client)    
             else:
-                login_error_show(2)# Wrong password
-        else:
-            login_error_show(1)# Username not found
+                login_error_show(2)# Wrong password    
+                return    
+    login_error_show(1)# Username not found
     
 def login_error_show(what:int):
     """
@@ -101,7 +101,7 @@ def sign_up():
         sign up page
     """
 
-    global signup_frame,new_fname,new_lname,new_mname,new_email,new_dob,new_passwrd,new_cpasswrd,gvar,new_uname
+    global clicked_login,signup_frame,new_fname,new_lname,new_mname,new_email,new_dob,new_passwrd,new_cpasswrd,gvar,new_uname
 
     py_bank_title.grid(row=0,column=1,pady=(200,0))# places "Py Bank" title on top
 
@@ -199,6 +199,7 @@ def sign_up_data():
     data_passwrd=new_passwrd.get()
     data_confpasswrd=new_cpasswrd.get()
     data_gender=gvar.get()
+    data_balance=100
     """
     Places all the data into a list.
     """
@@ -211,8 +212,10 @@ def sign_up_data():
         signup_error_show(_check_data)# Shows respective error.
 
     else:# No error found.
+        all_data.append(data_balance)
         DataActions.signup_submit(all_data)# Adds data to Account table.
         messagebox.showinfo("Success","Submitted Sucessfully")
+        clicked_login()
         
 def signup_error_show(_where:list) -> None:
     """
@@ -248,7 +251,7 @@ def signup_error_show(_where:list) -> None:
     signup_error1=Label(signup_frame,text=_text + _where[2],fg='red',bg=BACKGROUND1)# error in sign up page
     signup_error1.grid(row=_row,column=1,columnspan=4,sticky='WN')
 
-def homepage():
+def homepage(client:list):
     """
         The homepage, after login is successfull.
     """
@@ -256,25 +259,27 @@ def homepage():
     homepage_PybankLogo=Label(win,image=my_img7)# Py bank logo in home page at the top left corner.
     homepage_PybankLogo.grid(row=0,column=0,rowspan=3)
 
-    Label(win,text=f"Welcome {logged_in_name},").grid(row=0,column=1)# Welcome message at top.
+    homepage_welcome_label=Label(win,text=f"Welcome {logged_in_name},")# Welcome message at top.
+    homepage_welcome_label.grid(row=0,column=1)
 
     homepage_frame1=LabelFrame(win)
     homepage_frame1.grid(row=1,column=1)
     Label(homepage_frame1,text="Account username").grid(row=0,column=0)
 
-    homepage_frame2=LabelFrame(win,borderwidth=5)
+    homepage_frame2=LabelFrame(win,borderwidth=5)# Account Username
     homepage_frame2.grid(row=2,column=1,sticky="N")
-    Label(homepage_frame2,text='XXXXXXXXXX').grid(row=0,column=0)
+    Label(homepage_frame2,text=client[6]).grid(row=0,column=0)
 
     homepage_frame3=LabelFrame(win)
     homepage_frame3.grid(row=1,column=2)
     Label(homepage_frame3,text="Account Balance").grid(row=0,column=0)
 
-    homepage_frame4=LabelFrame(win)
+    homepage_frame4=LabelFrame(win)# Account Balance
     homepage_frame4.grid(row=2,column=2,sticky="N")
-    Label(homepage_frame4,text='20,000').grid(row=0,column=0)
+    Label(homepage_frame4,text=f'Rs. {client[8]}').grid(row=0,column=0)
 
-    Button(win,text="refresh").grid(row=1,column=3)# Refreah button
+    homepage_refresh=Button(win,text="refresh")# Refresh button
+    homepage_refresh.grid(row=1,column=3)
 
     homepage_frame5=LabelFrame(win)# Transaction Frame
     homepage_frame5.grid(row=3,column=2)
@@ -309,11 +314,26 @@ def homepage():
 
     Button(homepage_frame6,text="load").grid(row=2,column=0,columnspan=2)# load button
 
-    Button(win,text='sign out').grid(row=4,column=3)
+    def clicked_signout():
+        """
+            Goes back to login page. 
+        """
+        homepage_frame1.grid_forget()
+        homepage_frame2.grid_forget()
+        homepage_frame3.grid_forget()
+        homepage_frame4.grid_forget()
+        homepage_frame5.grid_forget()
+        homepage_frame6.grid_forget()
 
+        homepage_refresh.grid_forget()
+        homepage_welcome_label.grid_forget()
+        homepage_PybankLogo.grid_forget()
+        homepage_signout.grid_forget()
 
+        login_page()
 
-
+    homepage_signout=Button(win,text='sign out',command=clicked_signout)
+    homepage_signout.grid(row=4,column=3)
 
 
 def main():
