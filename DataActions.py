f408dc1c819301e2290d3f386f2f2f204d87da0d
Sworup Timalsina <SworupTimalsina>
2023-02-20 14:59:30 +0545

    added edit function

--- DataActions.py
+++ DataActions.py
@@ -22,7 +22,7 @@ def configure() -> None:
         password text,
         balance integer,
         accnumber integer
-
+        
     )
     """)
     print("database created successfully")
@@ -48,7 +48,7 @@ def signup_submit(info:list) -> None:
     conn=sqlite3.connect(DATABASE)
     c=conn.cursor()
 
-    c.execute('INSERT INTO Account VALUES (:first_name,:middle_name,:last_name,:email,:dob, :gender, :username, :password, :balance)',{
+    c.execute('INSERT INTO Account VALUES (:first_name,:middle_name,:last_name,:email,:dob, :gender, :username, :password, :balance, :accnumber)',{
         'first_name':info[0],
         'middle_name':info[1],
         'last_name':info[2],
@@ -83,8 +83,57 @@ def retrieve_all() -> list:
 
     conn.close()
 
+def edit(info:list):
+    """
+        Edits an account's info.
+
+        info[0] : first_name
+        info[1] : middle_name
+        info[2] : last_name
+        info[3] : email
+        info[4] : dob
+        info[5] : gender
+        info[6] : username
+        info[7] : password
+        info[8] : balance
+        info[9] : accnumber
+    """
+    conn= sqlite3.connect(DATABASE)
+
+    c= conn.cursor()
+
+    c.execute("""UPDATE Account SET
+        first_name= :fname,
+        middle_name= :mname,
+        last_name= :lname,
+        email= :email,
+        dob= :dob,
+        gender= :gender,
+        username= :uname,
+        password=:passwrd,
+        balance=:balance,
+        accnumber=:accnumber
+        WHERE username= :uname""",{
+        'fname':info[0],
+        'mname':info[1],
+        'lname':info[2],
+        'email':info[3],
+        'dob':info[4],
+        'gender':info[5],
+        'uname':info[6],
+        'passwrd':info[7],
+        'balance':info[8],
+        'accnumber':info[9],
+    })
+    conn.commit()
+    conn.close()
+
+
 try:# Creates tables for new host machine.
     configure()
 except sqlite3.OperationalError:
     pass
 
+if __name__=="__main__":
+    a=('sofwarica', '', 'college', 'softwarica@email.com', '2023-02-08', 'male', 'softwarica', 'Softwarica@123', 200, 90701000, 1)
+    edit(a)
